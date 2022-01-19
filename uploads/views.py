4b46272fd0907d79dd413820d75ce356b88430e3
caco <caco@posteo.net>
2022-01-19 14:47:29 -0300

    Adds media dir related stuff; first backend version for uploading files

--- uploads/views.py
+++ uploads/views.py
@@ -1,5 +1,16 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 
 
 def home_page(request):
+    if request.method == 'POST' and request.FILES['cnab_file']:
+        file = request.FILES['cnab_file']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+
+        # TODO: parse file and save in the database
+
+        # TODO: remove file after saving in the database
+        # os.remove(settings.MEDIA_ROOT + '/' + filename)
+
     return render(request, 'uploads/home.html')
