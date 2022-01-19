bc3d78c9a491b10bf7b2428c4f7ed93b6d7e48d2
caco <caco@posteo.net>
2022-01-19 13:39:13 -0300

    Creates basic elements to make the file upload

--- functional_tests/test_cnab.py
+++ functional_tests/test_cnab.py
@@ -5,5 +5,10 @@ class Cnab(FunctionalTest):
     def test_basic_page_elements(self):
         self.browser.get(self.live_server_url)
 
-        upload_head = self.browser.find_element_by_tag_name('h3').text
-        self.assertEqual('Upload CNAB file', upload_head)
+        # Head text indicating to upload de file
+        main_head = self.browser.find_element_by_tag_name('h3').text
+        self.assertEqual('Upload CNAB file', main_head)
+
+        # Has a button to upload the file
+        button_text = self.browser.find_element_by_tag_name('button').text
+        self.assertEqual('Upload', button_text)
