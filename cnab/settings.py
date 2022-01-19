fe00d1587ed1e44282cf881fdaa4e5074803b3e3
caco <caco@posteo.net>
2022-01-19 11:03:04 -0300

    Configures PostgreSQL for Django

--- cnab/settings.py
+++ cnab/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'cnab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cnab',
+        'USER': 'cnab',
+        'PASSWORD': 'cnab123',
+        'HOST': 'localhost'
     }
 }
 
