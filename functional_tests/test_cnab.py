f2395248929b74f740bf3b27c54ddce9257e56bc
caco <caco@posteo.net>
2022-01-26 12:16:22 -0300

    Adds functional test to test upload bad file error message (adding a sample file in file system)

--- functional_tests/test_cnab.py
+++ functional_tests/test_cnab.py
@@ -1,3 +1,7 @@
+import os
+
+from django.conf import settings
+
 from functional_tests.base import FunctionalTest
 
 
@@ -13,3 +17,14 @@ class Cnab(FunctionalTest):
         # Has a button to upload the file
         button_text = self.browser.find_element_by_tag_name('button').text
         self.assertEqual('Upload', button_text)
+
+    def test_upload_wrong_content_file_displays_message(self):
+        self.browser.get(self.live_server_url)
+
+        file_path = os.path.join(settings.BASE_DIR, 'functional_tests',
+                                 'bad_cnab_file.txt')
+        self.browser.find_element_by_name('cnab_file').send_keys(file_path)
+        self.browser.find_element_by_tag_name('button').click()
+
+        message = self.browser.find_element_by_id('message').text
+        self.assertIn('Formato do arquivo inválido.', message)
